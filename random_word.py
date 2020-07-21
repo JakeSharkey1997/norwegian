828d397573a0d6c7f2458fec92f9ca9ce8f319f3
Jake Sharkey <68150346+JakeSharkey1997@users.noreply.github.com>
2020-07-21 13:57:27 +0100

    Refactoring and renaming of functions and values

--- random_word.py
+++ random_word.py
@@ -2,48 +2,48 @@ import random
 from googletrans import Translator
 
 
-def random_word(updated_list):
+def random_word_from_list(updated_list):
     picked_word = random.choice(updated_list)
     return picked_word
 
 
-def generate_words():
+def inputted_number_of_words():
     number_of_words_str = input("How many words do you want to see? ")
     counter = int(number_of_words_str)
     return counter
 
 
-def chosen_words(counter, updated_list):
+def list_of_norsk_words(counter, updated_list):
     list_of_words = []
     while counter is not 0:
-        list_of_words.append(random_word(updated_list))
+        list_of_words.append(random_word_from_list(updated_list))
         counter -= 1
     return list_of_words
 
 
-def remove_syntax(words):
+def remove_syntax_from_file(words_file):
     words_updated = []
-    for word in words:
+    for word in words_file:
         words_updated.append(word[:-1])
     return words_updated
 
 
 def translate(norsk_words, translator):
-    translation_list = []
+    list_of_translated_words = []
     translations = translator.translate(norsk_words, src='no', dest='en')
     for translation in translations:
         if translation.origin != translation.text:
-            translation_list.append([translation.origin, translation.text])
-    return translation_list
+            list_of_translated_words.append([translation.origin, translation.text])
+    return list_of_translated_words
 
 
 if __name__ == "__main__":
     with open('norsk.txt', 'r') as file:
-        words = file.readlines()
-        updated_list = remove_syntax(words)
-        number = generate_words()
-        norsk_words = chosen_words(number*3, updated_list)
+        words_file = file.readlines()
+        updated_list = remove_syntax_from_file(words_file)
+        number = inputted_number_of_words()
+        norsk_words = list_of_norsk_words(number * 3, updated_list)
         translator = Translator()
         translation_list = translate(norsk_words, translator)
-        for translation in translation_list[:number]:
-            print(translation[0] + " --> " + translation[1])
+        for words in translation_list[:number]:
+            print(words[0] + " --> " + words[1])
